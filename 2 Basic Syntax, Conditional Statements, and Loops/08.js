bb0423c0642423fbed3d15eb7be316edf2d41765
Peter Lazarov <peter_lazarov@hotmail.com>
2023-05-19 23:59:30 +0300

    Create 08.js

---
+++ 2 Basic Syntax, Conditional Statements, and Loops/08.js
@@ -0,0 +1,9 @@
+function multiplicationTable(n){
+    for(let i = 1; i < 11; i++){
+        let multiply = n * i;
+        console.log(`${n} X ${i} = ${multiply}`);
+    }
+}
+
+multiplicationTable(5);
+multiplicationTable(2);
