38182a424db7315b79dab476b338b85e0107f3e0
Peter Lazarov <peter_lazarov@hotmail.com>
2023-05-25 21:05:02 +0300

    Name and one exercise

---
+++ 4 Data types/02. Chars to String.js
@@ -0,0 +1,16 @@
+function charsToString(one, two, three){
+    let asString = String(one + two + three);
+    console.log(asString);
+}
+
+charsToString('a',
+'b',
+'c');
+
+charsToString('%',
+'2',
+'o')
+
+charsToString('1',
+'5',
+'p')
