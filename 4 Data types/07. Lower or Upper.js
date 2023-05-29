4421133e86d8bc1b12dabfbbb9c3bfa6669f7b3d
Peter Lazarov <peter_lazarov@hotmail.com>
2023-05-29 23:06:58 +0300

    Create 07. Lower or Upper.js

---
+++ 4 Data types/07. Lower or Upper.js
@@ -0,0 +1,9 @@
+function lowerOrUpperCase(letter){
+    if (letter.toUpperCase() === letter) {
+        console.log('upper-case');
+      } else {
+        console.log('lower-case');
+      }
+}
+
+lowerOrUpperCase('l');
