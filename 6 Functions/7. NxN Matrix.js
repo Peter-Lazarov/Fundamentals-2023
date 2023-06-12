94731de380e31ea56d92616f87fdbaf97a53b605
Peter Lazarov <peter_lazarov@hotmail.com>
2023-06-12 22:11:41 +0300

    Create 7. NxN Matrix.js

---
+++ 6 Functions/7. NxN Matrix.js
@@ -0,0 +1,14 @@
+function nxnMatrix(number){
+    
+    for(let i = 0; i < number; i++){
+        let line = "";
+        for(let j = 0; j < number; j++){
+            line += number + " ";
+        }
+        console.log(line);
+    }
+}
+
+nxnMatrix(3);
+nxnMatrix(7);
+nxnMatrix(2);
