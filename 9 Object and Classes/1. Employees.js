a9e3db7a77850d56a74bf574af42aad45892052a
Peter Lazarov <peter_lazarov@hotmail.com>
2023-06-27 18:37:09 +0300

    Create 1. Employees.js

---
+++ 9 Object and Classes/1. Employees.js
@@ -0,0 +1,22 @@
+function experience(inputArray){
+    
+    let person = {};
+
+    for(const element of inputArray){
+        person[element] = element.length;
+    }
+
+    for (const key in person) {
+        console.log(`Name: ${key} -- Personal Number: ${person[key]}`);
+    }
+}
+
+experience(['Silas Butler', 'Adnaan Buckley', 'Juan Peterson', 'Brendan Villarreal']);
+
+
+// Name: Silas Butler -- Personal Number: 12
+// Name: Adnaan Buckley -- Personal Number: 14
+// Name: Juan Peterson -- Personal Number: 13
+// Name: Brendan Villarreal -- Personal Number: 18
+
+//"Name: {employeeName} -- Personal Number: {personalNum}"
