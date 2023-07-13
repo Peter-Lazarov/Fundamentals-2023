2312bec278a1eb657ac47b80f87ee1623104ed5d
Peter Lazarov <peter_lazarov@hotmail.com>
2023-07-13 19:16:45 +0300

    Create 3. Piccolo.js

---
+++ 10 Associative Arrays/3. Piccolo.js
@@ -0,0 +1,33 @@
+function piccolo(allWordsString){
+    let parking = new Map();
+    
+    for (const line of allWordsString) {
+        let [action, carNumber] = line.split(", ");
+        if(action == "IN"){
+            parking.set(carNumber);
+        }else{
+            parking.delete(carNumber);
+        }
+    }
+
+    let carAllNumbers = Array.from(parking.keys()).sort((a, b) => a.localeCompare(b));
+
+    if (parking.size == 0) {
+        console.log("Parking Lot is Empty");
+    }else{
+        for (const numbers of carAllNumbers) {
+            console.log(numbers);
+        }
+    }
+}
+
+piccolo(['IN, CA2844AA',
+'IN, CA1234TA',
+'OUT, CA2844AA',
+'IN, CA9999TT',
+'IN, CA2866HI',
+'OUT, CA1234TA',
+'IN, CA2844AA',
+'OUT, CA2866HI',
+'IN, CA9876HH',
+'IN, CA2822UU']);
